3e0406dd47b37d9a74fc27eff7ae063e6b608ebd
Ricardo <ricardodiaas19@gmail.com>
2019-07-03 18:27:31 +0100

    adicionei_umcalendario_afragmentcalendario_falta_adicionar_eventos

--- app/src/main/java/com/example/chirag/googlesignin/fragment/CalendarioFragment.java
+++ app/src/main/java/com/example/chirag/googlesignin/fragment/CalendarioFragment.java
@@ -4,11 +4,13 @@ package com.example.chirag.googlesignin.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -27,13 +29,10 @@ import io.realm.RealmResults;
  * A simple {@link Fragment} subclass.
  */
 public class CalendarioFragment extends FragmentGenerico {
-
-    @BindView(R.id.viewdisciplinas)
-    Button viewdisciplinas;
-
-    @BindView(R.id.llayout)
-    LinearLayout layout;
-
+    @BindView(R.id.calender)
+    CalendarView calender;
+    @BindView(R.id.date_view)
+    TextView date_view;
 
     Unbinder unbinder;
     OnMessageReadListener messageReadListener;
@@ -46,7 +45,7 @@ public class CalendarioFragment extends FragmentGenerico {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        unbinder1.unbind();
+
     }
 
 
@@ -54,14 +53,15 @@ public class CalendarioFragment extends FragmentGenerico {
         public void OnMessageRead(String message);
 
     }
-
+/*
     @OnClick(R.id.viewdisciplinas)
     public void onClicked() {
         ReadDatadiscilinas();
         messageReadListener.OnMessageRead("daniel");
 
     }
-
+*/
+/*
     public void ReadDatadiscilinas() {
         Disciplina s = new Disciplina(context);
         Realm o = s.getRealm();
@@ -85,14 +85,43 @@ public class CalendarioFragment extends FragmentGenerico {
         }
         // display.setText(builder.toString());
     }
+*/
+public void Calendardate(){
+    calender
+            .setOnDateChangeListener(
+                    new CalendarView
+                            .OnDateChangeListener() {
+                        @Override
+                        // In this Listener have one method
+                        // and in this method we will
+                        // get the value of DAYS, MONTH, YEARS
+                        public void onSelectedDayChange(
+                                @NonNull  CalendarView view,
+                                int year,
+                                int month,
+                                int dayOfMonth)
+                        {
 
+                            // Store the value of date with
+                            // format in String type Variable
+                            // Add 1 in month because month
+                            // index is start with 0
+                            String Date
+                                    = dayOfMonth + "-"
+                                    + (month + 1) + "-" + year;
+
+                            // set this date in TextView for Display
+                            date_view.setText(Date);
+                        }
+                    });
+}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calendario, container, false);
         unbinder1 = ButterKnife.bind(this, view);
-
+        Calendardate();
         return view;
     }
 
