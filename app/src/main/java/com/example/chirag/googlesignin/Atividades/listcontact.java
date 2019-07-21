566f6d0237a3d832f17ee714512d3378dc941273
dan <danielmartins8@hotmail.com>
2019-07-21 17:29:10 +0100

    upd 3

---
+++ app/src/main/java/com/example/chirag/googlesignin/Atividades/listcontact.java
@@ -0,0 +1,13 @@
+package com.example.chirag.googlesignin.Atividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listcontact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listcontact);
+    }
+}
