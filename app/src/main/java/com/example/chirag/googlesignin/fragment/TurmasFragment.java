31a16d32b2c081100b161bdde0cc0adabc2964ad
dan <danielmartins8@hotmail.com>
2019-07-22 19:47:36 +0100

    contacto a funcionar

--- app/src/main/java/com/example/chirag/googlesignin/fragment/TurmasFragment.java
+++ app/src/main/java/com/example/chirag/googlesignin/fragment/TurmasFragment.java
@@ -1,18 +1,24 @@
 package com.example.chirag.googlesignin.fragment;
 
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.chirag.googlesignin.Atividades.listcontact;
 import com.example.chirag.googlesignin.Entidades.Aula;
@@ -20,6 +26,7 @@ import com.example.chirag.googlesignin.Entidades.Contacto;
 import com.example.chirag.googlesignin.Entidades.Curso;
 import com.example.chirag.googlesignin.Entidades.Disciplina;
 import com.example.chirag.googlesignin.Entidades.Turma;
+import com.example.chirag.googlesignin.Outros.Useful;
 import com.example.chirag.googlesignin.R;
 import com.example.chirag.googlesignin.model.AulaModel;
 import com.example.chirag.googlesignin.model.CursoModel;
@@ -27,6 +34,9 @@ import com.example.chirag.googlesignin.model.DisciplinaModel;
 import com.example.chirag.googlesignin.model.TurmaModel;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,6 +46,8 @@ import io.realm.Realm;
 import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.RealmResults;
+import io.realm.com_example_chirag_googlesignin_model_CursoModelRealmProxy;
+import io.realm.com_example_chirag_googlesignin_model_TurmaModelRealmProxy;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -91,207 +103,6 @@ public class TurmasFragment extends FragmentGenerico {
     }
 
 
-    public void ReadDataaulas() {
-        Aula s = new Aula(context);
-        Realm o = s.getRealm();
-        RealmResults<AulaModel> results2 = o.where(AulaModel.class).findAll();
-        StringBuilder builder = new StringBuilder();
-        int WrapWidth = LinearLayout.LayoutParams.MATCH_PARENT;
-        int WrapHeight = LinearLayout.LayoutParams.WRAP_CONTENT;
-
-        ArrayList<TextView> mTextViewList = new ArrayList<>();
-        layout.removeAllViews();
-        for (AulaModel l : results2) {
-
-            TextView textView = new TextView(context);              //dynamically create textview
-            textView.setLayoutParams(new LinearLayout.LayoutParams(             //select linearlayoutparam- set the width & height
-                    ViewGroup.LayoutParams.MATCH_PARENT, 48));
-            textView.isTextSelectable();
-            textView.setText("ID:" + l.getID() + ";" + "Sumario:" + l.getSumario() + ";" + "Data:" + l.getDataDeOcorrencia());
-            textView.setOnClickListener(onclicklistener);
-
-            layout.addView(textView);
-            //     builder.append("id: " +l.getID()+" "+"Nome: "+l.getSumario()+"\n");
-        }
-        //  display.setText(builder.toString());
-    }
-
-    View.OnClickListener onclicklistener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (v instanceof TextView) {
-                String o = ((TextView) v).getText().toString();
-                String[] parts = o.split(":");
-
-                String[] parts2 = parts[1].split(";");
-
-
-                Disciplina s = new Disciplina(context);
-                Realm realm = s.getRealm();
-
-                String ole = parts2[0];
-                Integer kkkk = Integer.parseInt(ole);
-
-                realm.beginTransaction();
-
-
-                DisciplinaModel disc = realm.where(DisciplinaModel.class)
-                        .equalTo("Nome", spinner.getSelectedItem().toString()).findFirst();
-                if (disc == null) {
-                    realm.cancelTransaction();
-                }
-
-                AulaModel f = realm.where(AulaModel.class)
-                        .equalTo("ID", kkkk).findFirst();
-                if (f == null) {
-                    realm.cancelTransaction();
-                }
-                RealmList<AulaModel> ll = new RealmList<AulaModel>();
-                ll.add(f);
-                realm.insertOrUpdate(disc);
-                realm.commitTransaction();
-
-                ((TextView) v).setText("Aula foi adicionada com sucesso");
-
-            }
-
-        }
-    };
-
-
-    public void ReadDatadiscilinas() {
-        Disciplina s = new Disciplina(context);
-        Realm o = s.getRealm();
-        RealmResults<DisciplinaModel> results2 = o.where(DisciplinaModel.class).findAll();
-        StringBuilder builder = new StringBuilder();
-        layout.removeAllViews();
-        for (DisciplinaModel l : results2) {
-
-            TextView textView = new TextView(context);              //dynamically create textview
-            textView.setLayoutParams(new LinearLayout.LayoutParams(             //select linearlayoutparam- set the width & height
-                    ViewGroup.LayoutParams.MATCH_PARENT, 48));
-//        textView.setGravity(Gravity.CENTER_VERTICAL);//set the gravity too
-            //       textView.setId(l.getID());
-            textView.isTextSelectable();
-            textView.setText("id: " + l.getID() + " " + "Nome:" + l.getNome() + "\n");
-            //   textView.setOnClickListener(onclicklistener);
-
-            layout.addView(textView);
-
-
-        }
-        // display.setText(builder.toString());
-    }
-
-    public void PopulateSpinners() {
-        ArrayList<Integer> ano = new ArrayList<Integer>();
-        Disciplina s = new Disciplina(context);
-        Realm o = s.getRealm();
-        RealmResults<DisciplinaModel> results = o.where(DisciplinaModel.class).findAll();
-        for (DisciplinaModel l : results) {
-//            if (l.getAnolectivo() == null) {
-//                //  l.setNome("NULO");
-//            } else {
-//                ano.add(l.getAnolectivo());
-//            }
-
-        }
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(context, android.R.layout.simple_spinner_dropdown_item, ano);
-
-
-        spinner2.setAdapter(adapter);
-
-        ArrayList<String> disciplinas = new ArrayList<String>();
-        Integer anus = (Integer) spinner2.getSelectedItem();
-       /*
-       if(anus==null){
-           anus=20182019;
-       }
-       */
-
-//        RealmResults<DisciplinaModel> results2 = o.where(DisciplinaModel.getClass()).
-//                equalTo("Year", (Integer) spinner2.getSelectedItem()) //20182019
-//                .findAll();
-//        for (DisciplinaModel l : results2) {
-//            if (l.getNome() == null) {
-//                //  l.setNome("NULO");
-//            } else {
-//                disciplinas.add(l.getNome());
-//            }
-//
-//
-//        }
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, disciplinas);
-
-
-        spinner.setAdapter(adapter2);
-
-    }
-
-
-
-    @Override
-    public RealmObject CastRealmObjectToEntity(RealmObject obj) {
-        return null;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-//        null.unbind();
-        unbinder.unbind();
-    }
-
-    @Override
-    public boolean Validate() {
-        return false;
-    }
-
-    @Override
-    public void EntityToDOM() {
-
-    }
-
-    @Override
-    public void CleanView() {
-
-    }
-
-    @Override
-    public void SetEnable(boolean value) {
-
-    }
-
-    @Override
-    public Button getBtDelete() {
-        return null;
-    }
-
-    @Override
-    public Button getBtSave() {
-        return null;
-    }
-
-    @Override
-    public Button getBtNew() {
-        return null;
-    }
-
-    @Override
-    public Button getBtEdit() {
-        return null;
-    }
-
-    @Override
-    public Button getBtImport() {
-        return null;
-    }
-
-    @Override
-    public String getFragmentDesc() {
-        return "Turmas";
-    }
-
     @OnClick(R.id.btSaveTurma)
     public void saveOnClick() {
 
@@ -320,4 +131,164 @@ public class TurmasFragment extends FragmentGenerico {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @OnClick(R.id.btViewTurma)
+    public void viewOnClick() {
+        try {
+            Res getMany = GetAll(null);
+
+            AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(context);
+            View mView = getLayoutInflater().inflate(R.layout.combo_dialog, null);
+            dialogbuilder.setTitle("Selecione uma " + getFragmentDesc() + "!");
+
+            Spinner mySpinner = (Spinner) mView.findViewById(R.id.firstSpinnerDialog);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, getMany.getTxt());
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            mySpinner.setAdapter(adapter);
+
+            dialogbuilder.setPositiveButton("Ok", (dialog, which) -> {
+
+                if (mySpinner.getSelectedItem() != null) {
+
+                    Integer id = Useful.SplitIdFromDescription(mySpinner.getSelectedItem().toString());
+
+                    //Find record by id
+                    Optional<RealmObject> res = getMany.getLst().stream().filter(elem -> CastRealmObjectToEntity(elem).getID() == id).findFirst();
+
+                    if (res != null) {
+                        currentEntity = CastRealmObjectToEntity(res.get());
+
+                        OnOkView();
+                    }
+                }
+
+                dialog.dismiss();
+            });
+
+            dialogbuilder.setNegativeButton("Dismiss", (dialog, which) -> dialog.dismiss());
+
+            dialogbuilder.setView(mView);
+            AlertDialog dialog = dialogbuilder.create();
+            dialog.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @OnClick(R.id.btDeleteTurma)
+    public void deleteOnClick() {
+        try {
+            Turma s = new Turma(context);
+            s.entidade = currentEntity;
+            s.Delete();
+
+            AfterDelete();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Res GetAll(Integer year) {
+        return GetAll(year, null);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Res GetAll(Integer year, Integer id) {
+        Turma s = new Turma(context);
+        s.entidade.setProfId(ProfId);
+        s.entidade.setYear(year == null ? Year : year);
+        List<RealmObject> lst = s.ReadAllByYear();
+        ArrayList<String> txt = new ArrayList<String>();
+
+        if (id != null)
+            lst = lst.stream().filter(p -> CastRealmObjectToEntity(p).getID() == id).collect(Collectors.toList());
+
+        lst.forEach((elem) -> {
+           TurmaModel obj = CastRealmObjectToEntity(elem);
+
+            if (obj.getDescricao() != null)
+                txt.add(Useful.ConcatIdAndDescription(obj.getID(), obj.getDescricao()));
+        });
+        return new Res(txt, lst);
+    }
+
+    @Override
+    public TurmaModel CastRealmObjectToEntity(RealmObject obj) {
+        return ((com_example_chirag_googlesignin_model_TurmaModelRealmProxy) obj);
+    }
+
+    @Override
+    public boolean Validate() {
+        String txt = "";
+
+        if (!txt.isEmpty()) {
+            Toast.makeText(context, txt, Toast.LENGTH_SHORT).show(); //Show shadow text
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void EntityToDOM() {
+        description.setText(currentEntity.getDescricao());
+
+        SetEnable(false);
+    }
+
+    public CursoModel CopyEntity(CursoModel oldEntity) {
+        CursoModel newModel = new CursoModel();
+        newModel.setProfId(ProfId);
+        newModel.setYear(Year);
+        newModel.setDescricao(oldEntity.getDescricao());
+
+        return newModel;
+    }
+
+    @Override
+    public void CleanView() {
+        description.setText("");
+
+        currentEntity = null;
+    }
+
+    @Override
+    public void SetEnable(boolean value) {
+        description.setEnabled(value);
+    }
+
+    @Override
+    public Button getBtDelete() {
+        return btDeleteTurma;
+    }
+
+    @Override
+    public Button getBtSave() {
+        return btSaveTurma;
+    }
+
+    @Override
+    public Button getBtNew() {
+        return null;
+    }
+
+    @Override
+    public Button getBtEdit() {
+        return null;
+    }
+
+    @Override
+    public Button getBtImport() {
+        return null;
+    }
+
+    @Override
+    public String getFragmentDesc() {
+        return "Curso";
+    }
 }
